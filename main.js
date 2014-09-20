dce1e17c961550fa605f11198d84ee60685e09ff
Igor Montagner <igordsm@gmail.com>
2014-09-19 22:35:31 -0300

    Initial version

---
+++ main.js
@@ -0,0 +1,34 @@
+function update_audio() {
+    var audioplayer = document.getElementById("audioplayer");
+    var src = "http://telechargement.rfi.fr.edgesuite.net/rfi/francais/audio/journaux/r001/journal_francais_facile_20h00_-_20h10_tu_";
+    var today = new Date();
+    console.log(today.getUTCHours());
+    if (today.getUTCHours() < 20 || (today.getUTCHours() == 20 && today.getUTCMinutes() < 20)) {
+        today -= 3600 * 24 * 1000;
+        today = new Date(today);
+    }
+    src += today.getFullYear();
+    if (today.getMonth() < 9) {
+        src += "0";
+    }
+    src += today.getMonth()+1;
+    src += today.getDate();
+    src += ".mp3";
+    console.log(src);
+    if (audioplayer.src != src) {
+        audioplayer.src = src;    
+    }
+}
+
+
+//audioplayer.addEventListener    
+
+/*var btnPlay = document.getElementById("btnPlay");
+btnPlay.addEventListener("click", function () {
+    audioplayer.play();
+});*/
+
+window.addEventListener("focus", update_audio);
+document.addEventListener("DOMContentLoaded", update_audio);
+console.log(FlurryAgent.startSession("8Z6Q227P5C8S6R5CGV8J"));
+console.log(FlurryAgent.logEvent("Start!!!"));
